a5c608f4ca4edaee35cb8fff7874648453678bd1
Shaun <shaun.d@live.com>
2025-07-11 15:28:21 +0300

    Update ClientListControl.xaml.cs

--- Client/Todos.Client.Orchestrator/Controls/ClientListControl.xaml.cs
+++ Client/Todos.Client.Orchestrator/Controls/ClientListControl.xaml.cs
@@ -1,4 +1,6 @@
+using System.Collections.Specialized;
 using System.Windows.Controls;
+using System.Windows;
 
 namespace Todos.Client.Orchestrator.Controls
 {
@@ -7,6 +9,30 @@ namespace Todos.Client.Orchestrator.Controls
         public ClientListControl()
         {
             InitializeComponent();
+            this.Loaded += ClientListControl_Loaded;
+        }
+
+        private void ClientListControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Subscribe to FilteredClients changes if DataContext is set
+            var vm = DataContext;
+            var prop = vm?.GetType().GetProperty("FilteredClients");
+            if (prop != null)
+            {
+                if (prop.GetValue(vm) is INotifyCollectionChanged filteredClients)
+                {
+                    filteredClients.CollectionChanged += FilteredClients_CollectionChanged;
+                }
+            }
+        }
+
+        private void FilteredClients_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            // Scroll to the last item if any
+            if (ClientListView.Items.Count > 0)
+            {
+                ClientListView.ScrollIntoView(ClientListView.Items[ClientListView.Items.Count - 1]);
+            }
         }
     }
 } 
