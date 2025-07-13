635bc5d7ea5f5271a2ee7f9534ef2d0d2aaa67e5
Shaun <shaun.d@live.com>
2025-07-13 16:07:59 +0300

    add button logic

---
+++ Client/TodDos.Ui/Sections/Tasks/AddTaskControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Todos.Ui.Sections.Tasks
+{
+    /// <summary>
+    /// Interaction logic for AddTaskControl.xaml
+    /// </summary>
+    public partial class AddTaskControl : UserControl
+    {
+        public AddTaskControl()
+        {
+            InitializeComponent();
+        }
+    }
+} 
