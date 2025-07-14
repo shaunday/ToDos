b2d7951dd4b5e46df851ad8205dab5b09dec52d7
Shaun <shaun.d@live.com>
2025-07-14 16:20:51 +0300

    repo connection string

---
+++ Server/ToDos.Repository/ConnectionStringAccess.cs
@@ -0,0 +1,27 @@
+﻿using DotNetEnv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDos.Repository
+{
+    internal static class ConnectionStringAccess
+    {
+        internal static string GetDbConnectionString()
+        {
+            var envPath = System.IO.Path.Combine(AppContext.BaseDirectory, ".env.Repository");
+            Env.Load(envPath);
+
+            string server = Environment.GetEnvironmentVariable("DB_SERVER");
+            string db = Environment.GetEnvironmentVariable("DB_NAME");
+            string user = Environment.GetEnvironmentVariable("DB_USER");
+            string pass = Environment.GetEnvironmentVariable("DB_PASS");
+
+            string connectionString = $"Server={server};Database={db};User Id={user};Password={pass};TrustServerCertificate=True;";
+
+            return connectionString;
+        }
+    }
+}
