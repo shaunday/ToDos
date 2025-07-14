c586e2f91de4242f3293c95849f9bbdf517def9b
Shaun <shaun.d@live.com>
2025-07-14 15:15:06 +0300

    cancel to cancel editing

--- Client/TodDos.Ui/Sections/Tasks/TasksView.xaml.cs
+++ Client/TodDos.Ui/Sections/Tasks/TasksView.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows.Controls;
+using System.Windows.Input;
+using Todos.Ui.ViewModels;
 
 namespace Todos.Ui.Sections
 {
@@ -7,6 +9,22 @@ namespace Todos.Ui.Sections
         public TasksView()
         {
             InitializeComponent();
+            this.PreviewKeyDown += TasksView_PreviewKeyDown;
+        }
+
+        private void TasksView_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                if (DataContext is TasksViewModel vm && vm.EditingTask != null && vm.EditingTask.IsEditing)
+                {
+                    if (vm.CancelEditCommand.CanExecute(vm.EditingTask))
+                    {
+                        vm.CancelEditCommand.Execute(vm.EditingTask);
+                        e.Handled = true;
+                    }
+                }
+            }
         }
     }
 } 
